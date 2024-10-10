13ad047306a1b6d81e4ae78101c5af60e826373a
eXeis-ixt <mjlickerlim56789@gmail.com>
2024-10-10 15:48:02 +0600

    Navbar and other code added

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Navbar } from '@/components';
 interface Props {
     children: React.ReactNode;
 }
@@ -7,6 +7,7 @@ interface Props {
 const MarketingLayout = ({ children }: Props) => {
     return (
         <div className="flex flex-col items-center w-full">
+            <Navbar />
             {children}
         </div>
     )
