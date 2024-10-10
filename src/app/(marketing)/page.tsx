ede846dc534a8cc044cecd1d27c0155e88e902db
eXeis-ixt <mjlickerlim56789@gmail.com>
2024-10-10 18:13:15 +0600

    Hero finished

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Wrapper, Container } from "@/components";
 import Image from "next/image";
-import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+import { ArrowRight, ChevronRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { BorderBeam } from "@/components/ui/border-beam";
 const HomePage = () => {
   return (
     <section className=" w-full relative flex items-center justify-center px-4 md:px-0 py-8">
@@ -39,11 +43,49 @@ const HomePage = () => {
                   Zero code, maximum speed. Make professional sites easy, fast
                   and fun while delivering best-in-class SEO, performance.
                 </p>
+
+                <div className="hidden md:flex relative items-center justify-center mt-8 md:mt-12 w-full">
+                  <Link
+                    href="#"
+                    className="flex items-center justify-center w-max rounded-full border-t border-foreground/30 bg-white/20 backdrop-blur-lg px-2 py-1 md:py-2 gap-2 md:gap-8 shadow-3xl shadow-background/40 cursor-pointer select-none"
+                  >
+                    <p className="text-foreground text-sm text-center md:text-base font-medium pl-4 pr-4 lg:pr-0">
+                      ✨ {"  "} Start building your dream website now!
+                    </p>
+                    <Button
+                      size="sm"
+                      className="rounded-full hidden lg:flex border border-foreground/20"
+                    >
+                      Get Started
+                      <ArrowRight className="w-4 h-4 ml-1" />
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="relative flex items-center py-10 md:py-20 w-full">
+                <div className="absolute top-1/2 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
+
+                <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl">
+                  <Image
+                    src="/assets/dashboard.svg"
+                    alt="banner image"
+                    width={1200}
+                    height={1200}
+                    quality={100}
+                    className="rounded-md lg:rounded-xl bg-foreground/10 shadow-2xl ring-1 ring-border"
+                  />
+
+                  <BorderBeam size={250} duration={12} delay={9} />
+                </div>
               </div>
             </div>
           </div>
         </Container>
       </Wrapper>
+
+      {/* How it works */}
+
     </section>
   );
 };
